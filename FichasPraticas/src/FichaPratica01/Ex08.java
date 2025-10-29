c1c069e2aee5e8773e6f65aa898bc5c3b97dbc68
RicardojcGomes <ricardojcgomes98@gmail.com>
2025-10-29 16:18:57 +0000

    FichaPratica01

--- FichasPraticas/src/FichaPratica01/Ex08.java
+++ FichasPraticas/src/FichaPratica01/Ex08.java
@@ -1,8 +1,50 @@
 package FichaPratica01;
 
+import java.util.Scanner;
+
 public class Ex08 {
     public static void main(String[] args) {
 
-        
+        //import do scanner
+        Scanner input = new Scanner(System.in);
+
+        //variações-
+        double musica1, musica2, musica3, musica4, musica5;
+
+
+        //musica1
+        System.out.print("Quantos minutos tem a 1º musica: ");
+        musica1 = input.nextDouble();
+        System.out.print("Quantos segundos tem a 1º musica: ");
+        musica1 = input.nextDouble();
+
+
+        //musica2
+        System.out.print("Quantos minutos tem a 2º musica: ");
+        musica2 = input.nextDouble();
+        System.out.print("Quantos segundos tem a 2º musica: ");
+        musica2 = input.nextDouble();
+
+
+        //musica3
+        System.out.print("Quantos minutos tem a 3º musica: ");
+        musica3 = input.nextDouble();
+        System.out.print("Quantos segundos tem a 3º musica");
+        musica3 = input.nextDouble();
+
+
+        //musica4
+        System.out.print("Quantos minutos tem a 4º musica:");
+        musica4 = input.nextDouble();
+        System.out.print("Quantos segundos tem a 4º musica:");
+        musica4 = input.nextDouble();
+
+
+        //musica5
+        System.out.println(" Quantos minutos tem a 5º musica: ");
+        musica5 = input.nextDouble();
+        System.out.println("Quantos segundos tem a 5º musica");
+        musica5 = input.nextDouble();
+
     }
 }
