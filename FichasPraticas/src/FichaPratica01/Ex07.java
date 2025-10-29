67c394d47915c5070ca84f841385ac47580e7d7f
RicardojcGomes <ricardojcgomes98@gmail.com>
2025-10-29 14:58:12 +0000

    FichaPratica01

--- FichasPraticas/src/FichaPratica01/Ex07.java
+++ FichasPraticas/src/FichaPratica01/Ex07.java
@@ -10,6 +10,26 @@ public class Ex07 {
 
         //produtos, preço, valor a pagar 10% desconto
 
+        double produto1, produto2, produto3, desconto, valor;
+
+        //valor dos produtos
+        System.out.print("Valor produto 1: ");
+        produto1 = input.nextDouble();
+
+        System.out.print("Valor produto 2: ");
+        produto2 = input.nextDouble();
+
+        System.out.print("Valor produto 3: ");
+        produto3 = input.nextDouble();
+
+        //desconto dos produtos
+        desconto = (produto1+produto2+produto3) * 0.1;
+
+        //Valor com desconto
+        valor =  (produto1+produto2+produto3) - desconto;
+        System.out.println("valor com desconto: " +valor);
+
+
 
 
 
---
+++ FichasPraticas/src/FichaPratica01/Ex08.java
@@ -0,0 +1,8 @@
+package FichaPratica01;
+
+public class Ex08 {
+    public static void main(String[] args) {
+
+        
+    }
+}
