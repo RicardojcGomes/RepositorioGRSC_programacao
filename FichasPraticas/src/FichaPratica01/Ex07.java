dab62c14a9f390f4e5b45876ba738c377d006f4a
RicardojcGomes <ricardojcgomes98@gmail.com>
2025-10-29 13:56:14 +0000

    Create Ex07.java

---
+++ FichasPraticas/src/FichaPratica01/Ex07.java
@@ -0,0 +1,21 @@
+package FichaPratica01;
+
+import java.util.Scanner;
+
+public class Ex07 {
+    public static void main(String[] args) {
+
+        //import do scanner
+    Scanner input = new Scanner(System.in);
+
+        //produtos, preço, valor a pagar 10% desconto
+
+        
+
+
+
+
+
+
+    }
+}
