f049082111f3bbccb6cb4ab98bd7b0dac5a7eaf3
RicardojcGomes <ricardojcgomes98@gmail.com>
2025-10-29 21:46:17 +0000

    FichaPratica01

--- FichasPraticas/src/FichaPratica01/Ex08.java
+++ FichasPraticas/src/FichaPratica01/Ex08.java
@@ -46,7 +46,7 @@ public class Ex08 {
         System.out.println("Quantos segundos tem a 5º musica");
         musica5 = input.nextDouble();
 
-        //nao faco ideia de como tirar aquilo 
+        //nao faco ideia de como tirar aquilo
 
     }
 }
